1d359ed5dea31f101a0b927c6ee0f356ce0460aa
Valentin <vmihaila333@gmail.com>
2022-11-04 21:52:24 +0200

    fisrt dive was ok, much to learn

---
+++ JS/javascript.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+// this is an inline comment
+
+/*
+this is a multiline 
+                    comment
+*/
+ // data types: undefined, null, bolean, string, symbol, number and object.
+
+ /*let message="Chris P. Bacon";
+ alert(message)*/
+ /*let user="Chris P. Bacon",
+     age="22",
+     message="Hello";
+*/
+let admin, name
+name="Jhon";
+admin=name;
+alert(admin)
