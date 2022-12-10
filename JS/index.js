ff203b1794d1f1c03618b613d60679222cf1a450
Valentin <vmihaila333@gmail.com>
2022-12-10 10:24:14 +0200

    done for now

--- JS/index.js
+++ JS/index.js
@@ -240,22 +240,48 @@
 
 ///////////////////////////////////////////////
 
-//EVENT LISTNERS///////////////////////////////
+//EVENT LISTENERS///////////////////////////////
+
+//element.addEventListener("click", function);
+
+// const buttonTwo = document.querySelector('.box-2');
+
+// function alertBtn () {
+//     alert('this works too')
+// };
+
+// buttonTwo.addEventListener("click", alertBtn);
+
+// //mouse over event
 
 
+// const newBackgroundColor = document.querySelector('.box-3');
+
+// function changeBackground(){
+//     newBackgroundColor.style.backgroundColor = 'red';
+// };
+
+// newBackgroundColor.addEventListener("mouseover", changeBackground);
+
+//reveal event
+
+const revealBtn = document.querySelector('.reveal-btn');
+
+const hiddenContent = document.querySelector('.hidden-content');
+
+function revealContent(){
+    if(hiddenContent.classList.contains('reveal-btn')
+    ) {
+        hiddenContent.classList.remove('reveal-btn')
+    }   else {
+        hiddenContent.classList.add('reveal-btn')
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
 
 
-
-
-
-
-
-
-
-
-
-
-
+// the end for now
 
 
 
