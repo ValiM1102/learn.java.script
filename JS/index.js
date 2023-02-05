2e014725d9f27ca7237c102e412b8d0cd524e5fb
Valentin <vmihaila333@gmail.com>
2023-02-05 12:48:26 +0200

    blackjack game updates

--- JS/index.js
+++ JS/index.js
@@ -63,14 +63,73 @@
 // }
 
 //blackjack
+
+// random numbers function
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min)
 }  
 
+// here are all the cards
 let firstCard = getRandomIntInclusive(1, 11)
 let secondCard = getRandomIntInclusive(1, 11)
 
-let sum = firstCard + secondCard
 
+// let randomIndex = getRandomIntInclusive(0, 1);
+
+let allCards = [firstCard, secondCard]
+let sum = allCards[0] + allCards[1]
+
+
+
+
+let hasBlackjack = false
+let isAlive = true
+let message
+let cards = document.querySelector("#cards-el")
+let score = document.querySelector("#score-el")
+let messageEl = document.querySelector("#message-el")
+
+// these functions start and render the game when play and draw is pressed
+function startGame (){
+  renderGame()
+}
+
+function renderGame(){
+  cards.textContent = "Cards: ";
+  for (let i = 0; i < allCards.length; i++){
+    cards.textContent += allCards[i] + " "
+  }
+  score.textContent = "Score: " + sum
+  
+  if (sum <= 20){
+  message = "Do you want to draw another card?"
+} else if(sum === 21){
+  message = "Congratulations! Blackjack!"
+  hasBlackjack = true
+} else {
+  message = "Sorry, you've lost!"
+  isAlive = false
+}
+  messageEl.textContent = message;
+  
+}
+
+// draw new card function
+
+
+
+function newCard(){
+  let card = getRandomIntInclusive(1, 11); 
+  allCards.push(card)
+  sum += allCards[2];
+  renderGame();
+}
+
+function getRandomCard(){
+  return 5
+}
+let random = getRandomCard()
+console.log(random)
