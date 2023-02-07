135a52b7a9457f35a476d41176323ee86ea2f8d1
Valentin <vmihaila333@gmail.com>
2023-02-07 21:30:49 +0200

    new project, chrome extension

--- JS/index.js
+++ JS/index.js
@@ -66,81 +66,125 @@
 
 // random numbers function
 
-function randomCards(){
-    let randomNum = Math.floor( Math.random()*13 ) + 1;
-    if(randomNum > 10){
-      return randomNum = 10
-    } else if(randomNum === 1){
-      return randomNum = 11
-    } else {
-      return randomNum
-    }
-}  
-/////player details
+// function randomCards(){
+//     let randomNum = Math.floor( Math.random()*13 ) + 1;
+//     if(randomNum > 10){
+//       return randomNum = 10
+//     } else if(randomNum === 1){
+//       return randomNum = 11
+//     } else {
+//       return randomNum
+//     }
+// }  
+// /////player details
 
-let player = {
-  name: "Vali",
-  chips: 100
-}
-let playerEL = document.querySelector(".player-el")
-playerEL.textContent = player.name + ": $" + player.chips
+// let player = {
+//   name: "Vali",
+//   chips: 100
+// }
+// let playerEL = document.querySelector(".player-el")
+// playerEL.textContent = player.name + ": $" + player.chips
 
-/////
-let allCards = []
-let sum = 0
-let hasBlackjack = false
-let isAlive = false
-let message
-let cards = document.querySelector("#cards-el")
-let score = document.querySelector("#score-el")
-let messageEl = document.querySelector("#message-el")
+// /////
+// let allCards = []
+// let sum = 0
+// let hasBlackjack = false
+// let isAlive = false
+// let message
+// let cards = document.querySelector("#cards-el")
+// let score = document.querySelector("#score-el")
+// let messageEl = document.querySelector("#message-el")
 
-// these functions start and render the game when play and draw is pressed
-function startGame (){
-  isAlive = true
-  let firstCard = randomCards();
-  let secondCard = randomCards();
-  allCards = [firstCard, secondCard]
-  sum = allCards[0] + allCards[1]
-  renderGame()
-}
+// // these functions start and render the game when play and draw is pressed
+// function startGame (){
+//   isAlive = true
+//   let firstCard = randomCards();
+//   let secondCard = randomCards();
+//   allCards = [firstCard, secondCard]
+//   sum = allCards[0] + allCards[1]
+//   renderGame()
+// }
 
-function renderGame(){
+// function renderGame(){
  
-  cards.textContent = "Cards: ";
-  for (let i = 0; i < allCards.length; i++){
-    cards.textContent += allCards[i] + " "
-  }
-  score.textContent = "Score: " + sum
+//   cards.textContent = "Cards: ";
+//   for (let i = 0; i < allCards.length; i++){
+//     cards.textContent += allCards[i] + " "
+//   }
+//   score.textContent = "Score: " + sum
   
-  if (sum <= 20){
-  message = "Do you want to draw another card?"
-} else if(sum === 21){
-  message = "Congratulations! Blackjack!"
-  hasBlackjack = true
-} else {
-  message = "Sorry, you've lost!"
-  isAlive = false
-}
-  messageEl.textContent = message;
-  return allCards;
-}
+//   if (sum <= 20){
+//   message = "Do you want to draw another card?"
+// } else if(sum === 21){
+//   message = "Congratulations! Blackjack!"
+//   hasBlackjack = true
+// } else {
+//   message = "Sorry, you've lost!"
+//   isAlive = false
+// }
+//   messageEl.textContent = message;
+//   return allCards;
+// }
 
-// draw new card function
+// // draw new card function
 
 
 
-function newCard(){
-  if(isAlive === true && hasBlackjack === false){
-  let card = randomCards();
-  sum += card;
-  allCards.push(card)
-  renderGame();
-}}
+// function newCard(){
+//   if(isAlive === true && hasBlackjack === false){
+//   let card = randomCards();
+//   sum += card;
+//   allCards.push(card)
+//   renderGame();
+// }}
+
+
+///blackjack
 
 ////
 
-let countries = ["Romania", "Poland", "Germany", "USA", "Bulgaria"]
-countries.pop()
+// let countries = ["Romania", "Polonia", "Serbia", "Bulgaria", "Moldova"]
 
-console.log(countries)
+// countries.pop()
+// //removes last element
+// countries.shift()
+// //removes first element
+// countries.push("Austria")
+// //adds element to the end
+// countries.unshift("Germania")
+// //adds element to the start
+// console.log(countries)
+
+////
+
+// let dayOfTheMonth = 11
+// let month = "February"
+// if(dayOfTheMonth === 11 && month === "February"){
+//   console.log("Special day today big boss")
+// }
+
+////
+
+// let hands = ["rock", "paper", "scissors"]
+
+// function randomHand(){
+//   let random = Math.floor(Math.random()*hands.length)
+//   console.log(hands[random]) 
+// }
+// randomHand()
+
+////
+
+// chrome extension
+
+
+
+
+
+
+
+
+
+
+
+// chrome extension
